71f5c09743e8a03dddd959ef15a4239ff1a9841a
Brittany Huber <brittanyleighhuber@gmail.com>
2020-01-17 19:43:01 -0700

    Added model

---
+++ models/burger.js
@@ -0,0 +1,23 @@
+var orm = require("../config/orm");
+
+var burger = {
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        });
+    },
+
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res){
+            cb(res);
+        });
+    },
+
+    updateOne: function(col, val, conditionCol, conditionVal, cb) {
+        orm.updateOne("burgers", col, val, conditionCol, conditionVal, function(res){
+            cb(res);
+        });
+    }
+}
+
+module.exports = burger;
