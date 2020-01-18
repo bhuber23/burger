a03669974c7d05acf74a199ab29a95fb8482b21a
Brittany Huber <brittanyleighhuber@gmail.com>
2020-01-17 19:31:35 -0700

    Added orm.js

---
+++ config/orm.js
@@ -0,0 +1,30 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+    selectAll: function(table, cb){
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [table], function(err, result){
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO ??(??) VALUES(?)";
+        connection.query(queryString, [table, cols, vals], function(err, result){
+            if (err) throw err;
+            cb(result);
+        });
+    },
+
+    updateOne: function(table, col, val, conditionCol, conditionVal, cb){
+        var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        connection.query(queryString, [table, col, val, conditionCol, conditionVal], function(err, result){
+            if (err) throw err;
+            cb(result);
+        });
+    }
+}
+
+module.exports = orm;
